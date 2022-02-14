d85de3d1be4f22e5553d1517950c2ca0d9a03ebc
docppp <29142757+docppp@users.noreply.github.com>
2022-02-14 12:54:53 +0100

    label test fix

--- tst/test_label.py
+++ tst/test_label.py
@@ -27,21 +27,13 @@ class TestLabel(unittest.TestCase):
     def test_static_label_render(self):
         label = Label(0, 0, COLORS.BLACK, font_type="monospace", font_size=15, static_txt="a")
         label.get_surface()
-        calls = [
-            call("a", COLORS.BLACK, "monospace", 15)
-        ]
-        TextManager().get_render.assert_has_calls(calls)
-        self.assertEqual(TextManager().get_render.call_count, len(calls))
+        TextManager().get_render.assert_called_once_with("a", COLORS.BLACK, "monospace", 15)
         self.assertEqual(TextManager().get_default_render.call_count, 0)
 
     def test_dynamic_label_render(self):
         label = Label(0, 0, COLORS.BLACK, font_type="monospace", font_size=15, dynamic_txt=self.dynamic_txt)
         label.get_surface()
-        calls = [
-            call(self.dynamic_txt(), COLORS.BLACK, "monospace", 15)
-        ]
-        TextManager().get_render.assert_has_calls(calls)
-        self.assertEqual(TextManager().get_render.call_count, len(calls))
+        TextManager().get_render.assert_called_once_with(self.dynamic_txt(), COLORS.BLACK, "monospace", 15)
         self.assertEqual(TextManager().get_default_render.call_count, 1)
 
 
